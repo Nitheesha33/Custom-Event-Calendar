52519790a5d125ea4381d1f752753f8e347703c8
Nitheesha33 <nitheeshaboggavarapu@gmail.com>
2025-05-27 16:14:13 +0530

    for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/Custom-Event-Calendar", // Required for deployment (GitHub Pages, Vercel custom path)
   server: {
-    host: '0.0.0.0',
-  }
-})
+    host: '0.0.0.0', // Useful for preview on LAN or deployment preview
+  },
+});
